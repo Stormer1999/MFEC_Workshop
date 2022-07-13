014353de9afc8aa6bbc970998a3e904f76275da7
Worabordin Chumsintaiphat <stormer@Worabordins-MacBook-Air.local>
2022-07-13 21:08:45 +0700

    implement custom-response

--- src/main/java/th/co/mfec/api/controller/user/UserController.java
+++ src/main/java/th/co/mfec/api/controller/user/UserController.java
@@ -1,13 +1,21 @@
 package th.co.mfec.api.controller.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import th.co.mfec.api.model.common.SuccessResponse;
+import th.co.mfec.api.model.user.UserRequest;
+import th.co.mfec.api.model.user.UserResponse;
+import th.co.mfec.api.service.UserService;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
 
   // http://localhost:8080/api/v1/user/register
 
@@ -19,5 +27,16 @@ public class UserController {
   @GetMapping("/test/{id}")
   public String test(@PathVariable String id) {
     return id;
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity<SuccessResponse<UserResponse>> register(
+      @RequestBody UserRequest userRequest) {
+    SuccessResponse<UserResponse> successResponse = new SuccessResponse<>();
+    UserResponse userResponse = userService.createUser(userRequest);
+    successResponse.setData(userResponse);
+
+    return ResponseEntity.ok(successResponse);
+    // return ResponseEntity.ok(new SuccessResponse<>(userService.createUser(userRequest)));
   }
 }
--- src/main/java/th/co/mfec/api/service/UserService.java
+++ src/main/java/th/co/mfec/api/service/UserService.java
@@ -1,6 +1,5 @@
 package th.co.mfec.api.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import th.co.mfec.api.entity.User;
 import th.co.mfec.api.model.user.UserRequest;
@@ -14,7 +13,6 @@ public class UserService {
 
   private final UserRepository userRepository;
 
-  @Autowired
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
